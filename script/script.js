5ac03865a89b799a3ae475aba394fca836d6cb55
Matias Tortoriello <85110239+MatiasTortoriello@users.noreply.github.com>
2021-09-14 21:53:15 -0300

    TEST

---
+++ script/script.js
@@ -0,0 +1,7 @@
+let nombre = prompt("Ingresá tu nombre");
+let apellido = prompt("Ingresá tu apellido");
+let edad = parseInt(prompt("Ingresá tu edad"));
+let lugar = prompt("¿De dónde sos?");
+
+mensaje = `Tu nombre es ${nombre} ${apellido}, tenés ${edad} años y sos de ${lugar}. ¿Viste cómo adiviné?`;
+alert(mensaje);
