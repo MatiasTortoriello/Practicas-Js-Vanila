e0cf23984a3124d8de8a7d8c50d4ad82cafb9dd6
Matias Tortoriello <matiastortoriello1991@gmail.com>
2023-02-07 23:52:55 -0300

    Acomodo de archivos

--- script/app.js
+++ script/app.js
@@ -187,3 +187,64 @@ for (let i = 1; i <= cantidad; i++) {
 // 4) Escriba una función que pida un número, pero sólo debe dejarte continuar si el número ingresado es impar.
 
 // 5) Escriba una función que pida un número distinto de cero y lo devuelva con un formato de dos decimales. Por ejemplo, ingreso 23.3426 y devuelve 23.34. Aplicar las validaciones.
+
+
+////////////////////////////////////////
+// PRACTICA DOM//
+////////////////////////////////////////
+
+//Importante: NO IR AL ARCHIVO INDEX.HTML Y AGREGAR CLASES, ID NI NADA QUE NOS PUEDA OTORGAR VENTAJA. La idea de estos ejercicios es que podamos aplicar muchas propiedades y si agregáramos id en todas las etiquetas sólo practicaríamos obtenerlas por id.
+
+// 1) Capturar al h1 (mediante su id titulo) e imprimirlo por consola.
+
+// 2) Imprimir por consola el id y la clase del h1 que capturamos en 1).
+
+// 3) Modificar el texto del h1, ahora queremos que diga "Primer título". Verificar que en el inspector figura el texto que agregamos dinámicamente, pero si hacemos clic derecho y ver código fuente de la página vemos que nos lo muestra original, sin modificaciones dinámicas.
+
+// 4) Imprimir el texto del h1 por consola (el original o el modificado, no importa).
+
+// 5) Pedir un numero por prompt, si lo escribe cambiar el texto del h1 por "Usted ha escrito un número, muchas gracias". En caso que escriba otra cosa que el título diga "No has escrito un número".
+
+// 6) Borrar el contenido del título, pedir tres palabras por prompt e ir agregándolas al primero. No es necesario validar, para no perder la idea.
+
+// 7) A partir del h1 capturado en los incisos anteriores, queremos seleccionar la etiqueta p que dice "Primer párrafo. Este es el primer texto" e imprimirla por consola.
+
+// 8) A partir del h1 capturado en los incisos anteriores, queremos seleccionar la etiqueta p que dice "Segundo párrafo. Este es el segundo texto" e imprimirla por consola.
+
+// 9) A partir de la etiqueta que seleccionamos en el inciso anterior, queremos seleccionar al strong que tiene dentro e imprimirlo por consola.
+
+// 10) A partir de la etiqueta que seleccionamos en el inciso 8), queremos seleccionar el texto precedente al strong.
+
+// 11) A partir de la etiqueta que seleccionamos en el inciso 8), mostrar por consola el div padre que la contiene.
+
+// 12) Agregar la clase "fondo" a la etiqueta del inciso 11) a partir de haber capturado el h1 con id "titulo".
+
+// 13) Mostrar por consola los div.
+
+// 14) Mostrar por consola los div y agregarles la clase "fondo".
+
+// 15) Seleccionar el h2 a partir de su clase. Luego intentar llegar al primer p y modificar el texto por "Llegué hasta acá desde el h2 de abajo".
+
+// 16) Insertar una etiqueta "p" que diga "Estoy antes de todos los p" entre el h1 y el primer p.
+
+// 17) Insertar todos los elementos del siguiente array en la lista que figura en el html. Importante no borrar los li que ya estaban previamente.
+
+// 18) Idem 17) pero los elementos items que estaban originalmente en el html no deben estar. Es decir, que se borre primero el ul antes de insertar los elementos.
+
+// 19) Dentro del último item de la lista queremos agregar una nueva lista con estas características
+/* 
+<ul class="lista">
+    <li class="lista__items">
+        <a class="lista__enlaces" href="#">Link</a>
+    </li>
+    <li class="lista__items">
+        <a class="lista__enlaces" href="#">Link</a>
+    </li>
+    <li class="lista__items">
+        <a class="lista__enlaces" href="#">Link</a>
+    </li>
+</ul> 
+//Hay una forma sufrida y otra más fácil para cuando hay semejante estructura.
+*/
+
+// 20) Eliminar el último li de la lista (es decir, el li que tenía una lista dentro en el 19 ya no lo queremos más).
